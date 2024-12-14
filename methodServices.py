133eee21123518d66b11233e00285e18a13edf51
berkayemikonel <emikonelberkay@gmail.com>
2024-12-14 16:15:28 +0300

    Get Request is finished. Post Request isn't finished. It gives a few errors but it will be finished soon

--- methodServices.py
+++ methodServices.py
@@ -16,23 +16,38 @@ HTTP/1.1 200 OK
 <!DOCTYPE html>
 <html>
     <head>
+    
         <meta charset="utf-8">
         <title>Socket Server</title>
+        
+        
     </head>
     <body>
         <div class="container">
             {self.customPage.page()}
         </div>
-        <div class="container">
-            <form action="/upload" method="post" enctype="multipart/form-data">
-                <p>name</p><input type="text" id="name"></input>
-                <label for="files"><p>\n</p>Birden fazla dosya seçin:</label><p>\n\n\n</p>
-                
-                <input type="file" id="files" name="files[]" multiple>
-                <button type="submit">Yükle</button>
+    <h1> POST İsteği Test Formu </h1>
+        <div class="container">                 
+            <form action="/" method="POST"  enctype="application/x-www-form-urlencoded">
+                <label for="name">İSİM:</label><input type="text" id="name" name="name" required></form>
+                <button type="submit" onclick="submitForm()" >Yükle</button>
             </form>
+            <script>
+            function submitForm() {{
+                var form = document.querySelector('form');
+                var name = document.getElementById('name').value;
+                // Burada JavaScript değişkeni ile POST isteği çıktısı oluşturuluyor
+                var response = 'POST isteği alındı. Veri: ' + name;
+                // Formu gönder
+                form.submit();
+            }}
+            </script>
         </div>
     </body>
 </html>
 """
+
+
+
+
         return response
