45ce9babc7718c58e3be2b68aaa9d849641ae1d9
berkayemikonel <emikonelberkay@gmail.com>
2024-12-21 19:10:00 +0300

    Get Request is finished. Post Request is finished.

--- core/utils/reciveFullRequest.py
+++ core/utils/reciveFullRequest.py
@@ -1,5 +1,7 @@
 
-def receive_full_request(client_socket, buffer_size=4096):
+
+def receive_full_request(client_socket, buffer_size = 4096):
+
     request_data = b""
     headers_received = False
     content_length = 0
@@ -14,12 +16,14 @@ def receive_full_request(client_socket, buffer_size=4096):
 
             for line in headers.split(b"\r\n"):
                 if line.startswith(b"Content-Length:"):
-                    content_length = int(line.split(b":")[1].strip())
+                    content_length = int(line.split(":")[1].strip())
                     break
 
-        if headers_received:
-            body = request_data.split(b"\r\n\r\n", 1)[1]
-            if len(body) >= content_length:
-                break
+                if headers_received:
+                    body = request_data.split(b"\r\n\r\n", 1)[1]
+                    if len (body) >= content_length:
+                        break
 
     return request_data
+
+ #Post isteği bitmiştir.
